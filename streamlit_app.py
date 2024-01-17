d978d0c808e7dbed6203b864ab0d821c7b19535c
ChuppeSnowflake <155669942+ChuppeSnowflake@users.noreply.github.com>
2024-01-17 10:06:44 -0500

    Update streamlit_app.py
    Cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents New Healhy Diner')
 
@@ -7,7 +11,6 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -20,7 +23,6 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
@@ -31,7 +33,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
@@ -47,9 +50,3 @@ if add_my_fruit:
   streamlit.write('Thanks for adding ' + add_my_fruit)
 sql_cmd = "insert into fruit_load_list values('from streamlit')"
 my_cur.execute(sql_cmd)
-
-import os
-print(sql_cmd)
-import sys
-sys.stdout.write(sql_cmd)
-sys.stdout.flush()

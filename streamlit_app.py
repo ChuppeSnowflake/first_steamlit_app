24be87575952cf06f2b2402650a6c7c831af6976
ChuppeSnowflake <155669942+ChuppeSnowflake@users.noreply.github.com>
2024-01-17 10:42:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,4 +70,4 @@ if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_button = insert_row_snowflake(add_my_fruit)
   my_cnx.close()
-  streamlit.text(back_from_function)
+  streamlit.text(back_from_button)

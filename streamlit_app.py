3c3df63530f3655c838e4a182dd87e940c0c0bab
ChuppeSnowflake <155669942+ChuppeSnowflake@users.noreply.github.com>
2024-01-17 09:49:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if add_my_fruit:
 sql_cmd = "insert into fruit_load_list values('from streamlit')"
 my_cur.execute(sql_cmd)
 import os
-os.write(1,bytes(sql_cmd, 'utf-8'))
+os.write(1,encode(sql_cmd))
 
